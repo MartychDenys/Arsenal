f3437ff39a34ad332afe3e9f11842adaf874af39
Dmytro Hanzhelo <d.hanzhelo@indevlab.com>
2021-05-21 08:08:54 +0300

    Prod API url and iOS data

--- lib/infrastructure/constants.dart
+++ lib/infrastructure/constants.dart
@@ -1,1 +1,2 @@
-const apiUrl = 'https://b24arsenal-strahovanie.s11.itua.in.ua/local/api';
+// const apiUrl = 'https://b24arsenal-strahovanie.s11.itua.in.ua/local/api';
+const apiUrl = 'https://cp.arsenal-strahovanie.com/local/api';
