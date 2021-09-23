49367fb6a26eb8905ecf238d27ca2c68a90098ea
denysmartych <martych88@gmail.com>
2021-09-23 16:44:50 +0300

    fix image_picker

--- lib/infrastructure/provide_conclusion/provide_conclusion_image.service.dart
+++ lib/infrastructure/provide_conclusion/provide_conclusion_image.service.dart
@@ -5,11 +5,11 @@ import 'package:image_picker/image_picker.dart';
 class ProvideConclusionImageService {
   Future<File> getImageGallery() async {
     final image = await ImagePicker().getImage(source: ImageSource.gallery);
-    return File(image.path);
+    return (image != null) ? File(image.path) : null;
   }
 
   Future<File> getImageCamera() async {
     final image = await ImagePicker().getImage(source: ImageSource.camera);
-    return File(image.path);
+    return (image != null) ? File(image.path) : null;
   }
 }
